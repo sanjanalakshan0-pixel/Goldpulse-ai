c281467e80708080c552044c9a8792760bcfdb11
sanjanalakshan0-pixel <sanjanalakshan0@gmail.com>
2026-09-11 02:34:29 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-const CACHE="goldpulse-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html","manifest.webmanifest","icon.svg"]))));
-self.addEventListener("fetch",e=>{
-  if(e.request.url.includes("api.gold-api.com")) return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+const C="gp-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg"]))));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
